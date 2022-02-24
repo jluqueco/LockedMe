39c16243a231fdff5854f0ee3826b763350c01eb
jluqueco <jluqueco@gmail.com>
2022-02-24 00:15:38 -0500

    folder validation

--- LockedMe.java
+++ LockedMe.java
@@ -1,5 +1,49 @@
 package lockedme;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class LockedMe {
+	private Path path1;
+	private Path defPath;
+	
+	public LockedMe(String path) {
+		defPath = Path.of("./Default");
+		
+		if(path.equals("-1")) {
+			path1 = defPath;
+		}else {
+			path1 = Path.of(path);
+			System.out.println(Files.exists(path1));
+		}
+	}
+	
+	/**
+	 * @return the path1
+	 */
+	public Path getPath1() {
+		return path1;
+	}
+
+	/**
+	 * @return the defPath
+	 */
+	public Path getDefPath() {
+		return defPath;
+	}
+
+	@Override
+	public String toString() {
+		for(int i = 0; i < path1.getNameCount(); i++) {
+			System.out.println("Element " + i + " is: " + path1.getName(i));
+		}
+		
+		return "LockedMe [path1=" + path1.getFileName() + ", defPath=" + defPath + "]";
+	}
+	
+	
 
 }
--- LockedMeMain.java
+++ LockedMeMain.java
@@ -1,10 +1,35 @@
 package lockedme;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class LockedMeMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Scanner sc = new Scanner(System.in);
+		String path = null;
+		LockedMe lock = null;
+		boolean ind = true;
+		
+		System.out.println("****Welcome to LockeMe.com****");
+		System.out.println("Please enter the path for the main folder or enter -1 to use a default one.");
+		//path = sc.next();
+		path = 	"./default";		//"/home/eclipse-workspace/LockedMe/src/lockedme/LockedMe.java";
+		
+		while(ind){
+			if(Files.exists(Path.of(path))) {
+				lock = new LockedMe(path);
+				ind = false;
+			}else {
+				System.out.println("invalid path, please try again.");
+				System.out.println("Please enter the path for the main folder or enter -1 to use a default one.");
+				path = "./Default";
+			}
+		}
+			
+		System.out.println(lock.toString());
 	}
 
 }
